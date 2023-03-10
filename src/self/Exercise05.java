884af42da651bb975b5d81f37f6f5c116a1b9a43
sein Hwang <hhhj0525@daum.net>
2023-03-10 23:22:31 +0900

    이러닝 5차시 수업 문제 코드

---
+++ src/self/Exercise05.java
@@ -0,0 +1,26 @@
+package self;
+
+public class Exercise05 { //class Car 표현
+	String model_name;
+	String engine;
+	int engine_displacement;
+	String fuel;
+	double fuel_effecient;
+
+	public void startUp() {
+
+	}
+
+	public void backUp() {
+
+	}
+
+	public void speedUp() {
+
+	}
+
+	public void speedDown() {
+
+	}
+
+}
