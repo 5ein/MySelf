edf70aef67913b68c197ff49952c55e7a57276d9
5ein <124111027+5ein@users.noreply.github.com>
2023-04-30 00:28:13 +0900

    코테문제 1문제 품
    숫자찾기 문제

--- Practice03/src/프로그래머스코테/숫자찾기.java
+++ Practice03/src/프로그래머스코테/숫자찾기.java
@@ -1,20 +1,28 @@
 package 프로그래머스코테;
 
-
 public class 숫자찾기 {
 
 	public static void main(String[] args) {
 		int num = 232443;
 		int k = 4;
 
-	    int answer = 0;
+	    int answer = -1;
 		
-		String num2 = String.valueOf(num);
-		String k2 = String.valueOf(k);
-		
-		if(num2.contains(k2)) answer = num2.indexOf(k2) + 1;
-		else answer = -1;
-		
+//		String num2 = String.valueOf(num);
+//		String k2 = String.valueOf(k);
+//		if(num2.contains(k2)) answer = num2.indexOf(k2) + 1;
+	    
+	    String num2 = Integer.toString(num);
+	    String[] num3 = num2.split("");
+	    String k2 = Integer.toString(k);
+	    
+	    for (int i = 0; i < num3.length; i++) {
+			if (num3[i].equals(k2)) {
+				answer = i + 1;
+				break;
+			}
+		}
+	    
 		System.out.println(answer);
 	}
 
